ca40f465fe53640424a77e0b042ab2d952692813
kaenpe <knapalke@gmail.com>
2021-01-06 01:35:59 +0100

    more zoom experiments

--- pages/_app.js
+++ pages/_app.js
@@ -38,7 +38,7 @@ const MyApp = ({ Component, pageProps }) => {
         <title>Burger Builder</title>
         <meta
           name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=0'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
         />
       </Head>
       <ThemeProvider theme={theme}>
